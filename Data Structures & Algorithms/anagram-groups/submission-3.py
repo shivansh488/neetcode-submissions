7701b33b5f6fb44a0c8a52d2b7a7a6034b57fbb3
shivansh mahajan <153850035+shivansh488@users.noreply.github.com>
2026-06-12 11:56:38 +0530

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+from typing import List
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = defaultdict(list)
+
+        for word in strs:
+            key = tuple(sorted(word))   # convert word → sorted tuple
+            groups[key].append(word)
+
+        return list(groups.values())
+
+
+        
+     
+
